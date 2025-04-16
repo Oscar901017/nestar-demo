dab186839636fc02a6b54cc7da9ff69a069e461e
unknown <shokirovbekzod6090@gmail.com>
2025-04-16 19:23:53 +0900

    feat:intefration Header and Footer via Layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,6 +1,20 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
+import { useState } from "react";
 
-const Community = () => {
-  return <div>COMMUNITY</div>;
+
+const Comunity:NextPage = () => {  
+  const [title, setTitle]= useState<string>("hello")
+
+  return (
+    <div>
+      COMUNITY{""}
+      <button onClick={() => alert("Hello MIT")} style={{margin:"15px"}}>
+      PressMe
+      </button>
+     
+    </div>
+  );
 };
 
-export default Community;
+export default (Comunity);
--- pages/property/[productId]/index.tsx
+++ pages/property/[productId]/index.tsx
@@ -1,9 +1,9 @@
-import { useRouter } from "next/router";
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Box, Container } from "@mui/material";
+import { NextPage } from "next";
 
-const PropertyDetail = () => {
-  const router = useRouter();
-  const { productId } = router.query;
-  return <div>PROPERTY DETAIL {productId}</div>;
+const PropertyList: NextPage = () => {
+  return <Container>PROPERTY LIST</Container>;
 };
 
-export default PropertyDetail;
+export default withLayoutBasic(PropertyList);
